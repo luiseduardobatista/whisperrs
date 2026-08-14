dcc54d4d7b687ecccc863179d25773ab63b05e88
Luís Eduardo Batista <luiseduardob303@gmail.com>
2026-08-14 10:52:32 -0300

    chore: bump version to 0.1.4

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     in {
       default = pkgs.rustPlatform.buildRustPackage {
         pname = "whisper";
-        version = "0.1.3";
+        version = "0.1.4";
         src = ./.;
         cargoLock.lockFile = ./Cargo.lock;
         nativeBuildInputs = [
